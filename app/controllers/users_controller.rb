50e2572d9a7189dbdb0e4ac9497334d0e0457d9a
Szymon <wilkszymon@yahoo.com>
2021-05-05 23:40:41 +0200

    fix a bug with sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
-    before_action :require_user  
+    before_action :require_user, only: [:index, :show, :edit, :destroy] 
 
 
 def new
